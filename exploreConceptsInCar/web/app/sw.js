2d81216f3f58f40a86574fbf25ebabd5fb82eeee
samszo <samszon@gmail.com>
2026-08-31 11:47:48 +0200

    feat: mettre à jour la version du cache du service worker et ajouter une règle CSS pour gérer l'attribut hidden

--- exploreConceptsInCar/web/app/sw.js
+++ exploreConceptsInCar/web/app/sw.js
@@ -7,7 +7,7 @@
  * général en ligne au premier lancement), cache en secours. Ça évite qu'un
  * déploiement reste invisible derrière un vieux cache.
  */
-const CACHE = 'flux-shell-v6';
+const CACHE = 'flux-shell-v7';
 const SHELL = [
   './',
   './index.html',
